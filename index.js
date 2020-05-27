80181d2b45dff6a17af9f433ca9c39be9cac6806
yinonhever <yinonehever@gmail.com>
2020-05-27 15:40:42 +0300

    adding testimonials and cta sections

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// RESPONDING TO CLICK ON MOBILE MENU ICON
 $("#toggler").change(() => {
     $("body").toggleClass("no-scroll");
 
@@ -8,3 +9,48 @@ $("#toggler").change(() => {
         $(".header__nav-icon").prop("src", "images/icon-hamburger.svg");
     }
 })
+
+
+// INITIALIZING THE SWIPERJS SLIDER
+$(document).ready(function () {
+    var mySwiper = new Swiper(".swiper-container", {
+        // Optional parameters
+        init: true,
+        slidesPerView: slides(),
+        spaceBetween: 30,
+        freeMode: setFreeMode(),
+
+        // Adding pagination
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true
+        },
+
+        // Adding scrollbar
+        scrollbar: {
+            el: ".swiper-scrollbar",
+            hide: true
+        },
+    });
+});
+
+// Setting the number of slides per view according to screen width
+function slides() {
+    if ($(window).width() > 1050) {
+        return 2.5;
+    }
+    else {
+        return 1;
+    }
+}
+
+
+// Setting free mode to the slider on desktop
+function setFreeMode() {
+    if ($(window).width() > 1050) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
